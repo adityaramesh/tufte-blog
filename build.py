627149515696c5528bfefc989abaf1b2656ceb63
Aditya Ramesh <_@adityaramesh.com>
2017-04-12 18:47:51 -0400

    Support for tables.

--- build.py
+++ build.py
@@ -76,6 +76,21 @@ def fix_citations(soup):
 		tag.name = 'cite'
 		del tag['class']
 
+def fix_fullwidth_tables(soup):
+	for tag in soup.find_all('table'):
+		for n in tag.next_siblings:
+			if n.name is not None:
+				break
+
+		if n is not None and n.name == 'span' and 'class' in n and \
+			n['class'][0] == 'fullwidth':
+
+			n.decompose()
+
+			if 'class' not in tag: tag['class'] = []
+			tag['class'].append('fullwidth')
+			del tag['style']
+
 def fix_blockquotes_with_footers(soup):
 	"""
 	Puts blockquotes with footers in the format expected by tufte-css.
@@ -132,6 +147,25 @@ def fix_blockquotes_with_footers(soup):
 
 		a = find_anchor(footer)
 		if a is not None: tag['cite'] = a['href']
+
+def denest_singleton_footnotes(soup):
+	for figure_tag in soup.find_all('figure'):
+		for child in figure_tag.children:
+			if child.name == 'p':
+				c = list(child.children)
+				if len(c) == 0 or len(c) > 2: continue
+
+				b1 = c[0].name == 'a' and 'footnoteRef' in c[0]['class']
+				b2 = len(c) == 1
+				b3 = len(c) == 2 and c[1].name == 'span' and \
+					'unnumbered' in c[1]['class'] 
+
+				if b1 and (b2 or b3):
+					for tag in c:
+						tag.extract()
+						child.insert_before(tag)
+
+					child.decompose()
 
 def convert_footnotes_to_sidenotes(soup):
 	footnotes_tag = soup.find('section', {'class': 'footnotes'})
@@ -208,10 +242,14 @@ def convert_footnotes_to_sidenotes(soup):
 
 def postprocess_html_file(path):
 	soup = BeautifulSoup(open(path, 'r'), 'html.parser')
+
 	fix_h2_subtitles(soup)
 	fix_code_listings(soup)
 	fix_citations(soup)
+	fix_fullwidth_tables(soup)
 	fix_blockquotes_with_footers(soup)
+
+	denest_singleton_footnotes(soup)
 	convert_footnotes_to_sidenotes(soup)
 
 	with open(path, 'w') as f:
@@ -255,17 +293,5 @@ for post_name in os.listdir('posts'):
 	subprocess.run(['pandoc', *global_pandoc_args, '-i', post_src_path, '-o', post_dst_path,
 		'--template=' + template_output_path])
 	postprocess_html_file(post_dst_path)
-
-"""
-os.mkdir('output/posts/tufte')
-shutil.copytree('posts/tufte/images', 'output/posts/tufte/images')
-
-input_path = 'posts/tufte/tufte.md'
-output_path = 'output/posts/tufte/tufte.html'
-
-subprocess.run(['pandoc', *global_pandoc_args, '-i', input_path, '-o', output_path,
-	'--template=' + template_output_path, '--variable', 'subtitle=Dave Liepmann'])
-postprocess_html_file(output_path)
-"""
 
 shutil.rmtree('temp')
